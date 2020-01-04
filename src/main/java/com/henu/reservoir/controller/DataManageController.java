c63f09507702aafd77ed0695b35a8e325c2169eb
bzdbzdbzd121 <kneight@foxmail.com>
2020-01-04 11:16:37 +0800

    Improve tables.html and DataManageController: now tables.html use ajax to get json from url:"api/data/img" in DataManageController. update the version of "datatables" plug-in to 1.10.20

--- src/main/java/com/henu/reservoir/controller/DataManageController.java
+++ src/main/java/com/henu/reservoir/controller/DataManageController.java
@@ -3,69 +3,145 @@ package com.henu.reservoir.controller;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.henu.reservoir.domain.OpticalImgDao;
+import com.henu.reservoir.domain.ReservoirInfoDao;
 import com.henu.reservoir.domain.SarImgDao;
 import com.henu.reservoir.service.OpticalImgService;
+import com.henu.reservoir.service.ReservoirInfoService;
 import com.henu.reservoir.service.SarImgService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 public class DataManageController {
     private SarImgService sarImgService;
     private OpticalImgService opticalImgService;
-
-    private ObjectMapper mapper = new ObjectMapper();
+    private ReservoirInfoService reservoirInfoService;
 
     @Autowired
     private void setService(
             SarImgService sarImgService,
-            OpticalImgService opticalImgService
+            OpticalImgService opticalImgService,
+            ReservoirInfoService reservoirInfoService
     ){
         this.sarImgService = sarImgService;
         this.opticalImgService = opticalImgService;
+        this.reservoirInfoService = reservoirInfoService;
     }
 
-    @GetMapping("api/data/sar")
-    @ResponseBody
-    public String getSarImageList(String key){
-        List<SarImgDao> list;
-        if (key==null) {
-            //无关键字
-            list = sarImgService.findAllSarImg();
+    private ObjectMapper mapper = new ObjectMapper();
+    private HashMap<Integer, ReservoirInfoDao> reservoirInfoDaoHashMap;
+
+    private void buildReservoirInfoDaoHashMap(){
+        List<ReservoirInfoDao> reservoirInfoDaoList = reservoirInfoService.findAllReservoirInfo();
+        reservoirInfoDaoHashMap = new HashMap<>();
+        for (ReservoirInfoDao dao:
+             reservoirInfoDaoList) {
+            reservoirInfoDaoHashMap.put(dao.getId(), dao);
         }
-        else {
-            list = sarImgService.findSarImgByKeyWord(key);
+    }
+
+    @GetMapping("api/data/image")
+    @ResponseBody
+    public String getSarImageList(){
+        buildReservoirInfoDaoHashMap();
+        List<SarImgDao> list1 = sarImgService.findAllSarImg();
+        List<OpticalImgDao> list2 = opticalImgService.findAllOpticalImg();
+        List<DownloadItem> dlist = new ArrayList<>();
+        for (SarImgDao dao:
+                list1) {
+            dlist.add(new DownloadItem(
+                    reservoirInfoDaoHashMap.get(dao.getreservoir_id()).getName(),
+                    dao.getPath(),
+                    new SimpleDateFormat("yyyy-MM-dd").format(dao.getDate()),
+                    "sar",
+                    dao.getId()
+            ));
+        }
+        for (OpticalImgDao dao:
+                list2) {
+            dlist.add(new DownloadItem(
+                    reservoirInfoDaoHashMap.get(dao.getreservoir_id()).getName(),
+                    dao.getPath(),
+                    new SimpleDateFormat("yyyy-MM-dd").format(dao.getDate()),
+                    "optical",
+                    dao.getId()
+            ));
         }
         try {
-            return mapper.writeValueAsString(list);
+            return mapper.writeValueAsString(dlist);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
         return "error";
     }
 
-    @GetMapping("api/data/optical")
-    @ResponseBody
-    public String getOpticalImageList(String key){
-        List<OpticalImgDao> list;
-        if (key==null) {
-            //无关键字
-            list = opticalImgService.findAllOpticalImg();
-        }
-        else {
-            list = opticalImgService.findOpticalImgByKeyWord(key);
-        }
-        try {
-            return mapper.writeValueAsString(list);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-        return "error";
-    }
+    private class DownloadItem {
+        private String rname;
 
+        public String getRname() {
+            return rname;
+        }
+
+        public void setRname(String rname) {
+            this.rname = rname;
+        }
+
+        public String getPath() {
+            return path;
+        }
+
+        public void setPath(String path) {
+            this.path = path;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public void setType(String type) {
+            this.type = type;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+
+        private String path;
+        private String date;
+        private String type;
+        private int id;
+
+        public DownloadItem(String rname, String path, String date, String type, int id) {
+            this.rname = rname;
+            this.path = path;
+            this.date = date;
+            this.type = type;
+            this.id = id;
+        }
+
+        public DownloadItem() {
+        }
+    }
 }
+
+
