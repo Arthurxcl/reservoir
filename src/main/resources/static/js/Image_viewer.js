bf25e001fde0b9fe56b4d7d9388fd01c62958ef0
bzdbzdbzd121 <kneight@foxmail.com>
2020-01-10 11:04:48 +0800

    Add mouse Overlay style for images using ImageViewer. Fix bugs of ImageViewer. Edit form.html.

--- src/main/resources/static/js/Image_viewer.js
+++ src/main/resources/static/js/Image_viewer.js
@@ -2,7 +2,7 @@ $("body").append("<div id=\"image_viewer\" onclick='function hide() {" +
     "          $(\"#image_viewer\").hide();" +
     "        }'>" +
     "            <div id=\"background\"></div>" +
-    "            <img src=\"\">" +
+    "            <img alt='ImageViewer' src=\"\">" +
     "        </div>");
 $("#image_viewer").on("click", function () {
     $("#image_viewer").fadeOut()
@@ -10,21 +10,23 @@ $("#image_viewer").on("click", function () {
 
 $("#image_viewer img").on('mousewheel DOMMouseScroll', onMouseScroll);
 function onMouseScroll(e){
+    let img = $("#image_viewer img");
     e.preventDefault();
     let wheel = e.originalEvent.wheelDelta || -e.originalEvent.detail;
     let delta = Math.max(-1, Math.min(1, wheel) );
-    let height = $("#image_viewer img").height();
-    $("#image_viewer img").css("max-height", "none");
-    $("#image_viewer img").css("max-width", "none");
-    $("#image_viewer img").css("transition", "0.2s");
+    let height = img.height();
+    img.css("max-height", "none");
+    img.css("max-width", "none");
+    img.css("transition", "0.2s");
 
     if(delta<0){//向下滚动
         height *= 0.8;
-        $("#image_viewer img").height(height);
+        img.height(height);
     }else{//向上滚动
         height *= 1.25;
-        $("#image_viewer img").height(height);
+        img.height(height);
     }
+    $("#background").height(Math.max(img.height(), $("#image_viewer").height()));
 }
 
 function view_image(img) {
@@ -33,6 +35,7 @@ function view_image(img) {
         .css("max-height", "90%")
         .css("max-width", "90%")
         .css("transition", "none");
+    $("#background").height("100%");
     $("#image_viewer").fadeIn();
 }
 
